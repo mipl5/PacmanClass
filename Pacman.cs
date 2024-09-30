4536b0288d5d77ad5fc9a32abf3e01d6e604769b
Michael <143253674+mipl5@users.noreply.github.com>
2024-09-30 17:46:24 +0300

    Update Pacman.cs

--- Pacman.cs
+++ Pacman.cs
@@ -1,49 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework
+public class Pacman
 {
-    public class Pacman
+    //members
+    private int xPlace;
+    private int yPlace;
+    private int lives;
+    private int points;
+    
+    //constructors
+    public Pacman(int xPlace, int yPlace)
     {
-        //members
-        private int xPlace;
-        private int yPlace;
-        private int lives;
-        private int points;
-        
-        //constructors
-        public Pacman(int xPlace, int yPlace)
+        this.xPlace = xPlace;
+        this.yPlace = yPlace;
+        lives = 3;
+        // points is by default 0.
+    }
+    public Pacman() : this(0, 0) { }
+
+    //methods
+
+    // public
+    public void MoveUp() => Move(0, 1);
+    public void MoveDown() => Move(0, -1);
+    public void MoveRight() => Move(1, 0);
+    public void MoveLeft() => Move(-1, 0);
+
+    public void Eat() => points++;
+    public void KillSkeleton() => points += 10;
+    public void Attacked() => lives--;
+
+    //pivate
+    private void Move(int dx, int dy)
+    {
+        if (lives > 0)
         {
-            this.xPlace = xPlace;
-            this.yPlace = yPlace;
-            lives = 3;
-            // points is by default 0.
-        }
-        public Pacman() : this(0, 0) { }
-
-        //methods
-
-        // public
-        public void MoveUp() => Move(0, 1);
-        public void MoveDown() => Move(0, -1);
-        public void MoveRight() => Move(1, 0);
-        public void MoveLeft() => Move(-1, 0);
-
-        public void Eat() => points++;
-        public void KillSkeleton() => points += 10;
-        public void Attacked() => lives--;
-
-        //pivate
-        private void Move(int dx, int dy)
-        {
-            if (lives > 0)
-            {
-                xPlace += dx;
-                yPlace += dy;
-            }
+            xPlace += dx;
+            yPlace += dy;
         }
     }
 }
